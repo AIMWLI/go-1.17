6652bd3a6c85e4dfa9976400955c27d95a10cf44
songjin <jin17.song@gmail.com>
2024-01-10 14:18:49 +0800

    info: send to msg to bot

--- service/LivenessProbes.go
+++ service/LivenessProbes.go
@@ -14,12 +14,12 @@ import (
 
 var tf = "2006-01-02 15:04:05"
 
-var resArray = mapset.NewSet("detection", "ai-nlp up", "pong")
+var resArray = mapset.NewSet("detection", "ai-nlp up", "pong", "hello world", "ok")
 
 func StartLivenessProbesJob() {
 	kv := setting.ProbesSetting.KV
 	c := cron.New(cron.WithSeconds())
-	c.AddFunc("@every 20s", func() {
+	c.AddFunc("@every 5s", func() {
 		client := util.GetHttpClient()
 		for k, v := range kv {
 			//fmt.Printf("Address of i=%d:\t%p\n", client, &client)
@@ -27,10 +27,12 @@ func StartLivenessProbesJob() {
 			if resp != nil {
 				defer resp.Body.Close()
 			}
-			if err != nil || resp.StatusCode != http.StatusOK {
-				fmt.Println(err)
+			if err != nil {
+				//fmt.Printf("%v %v", k, err)
 				fmt.Errorf("failed to fetch URL %s : %s", v, resp.Status)
-				SendMsgToBot("探针检测响应异常", k, v)
+			}
+			if resp.StatusCode != http.StatusOK {
+				SendMsgToBot("探针检测异常", k, v)
 				continue
 			}
 
@@ -47,27 +49,48 @@ func StartLivenessProbesJob() {
 	})
 	c.Start()
 }
+
+//receiveId["asr"] = "oc_aa573b380df9e8fde91a106f772dea35"
+//receiveId["ocr"] = "oc_bbaa1949e2267b73c3cc32e2e36d448d"
+
 func SendMsgToBot(msg string, k string, v string) {
-	botUrl := "http://xx.xx.xx.xx/send_to_feishu/send_text"
+	//receiveId["nlp"] = "oc_532f1e2b627dd390434eba76cbe2b79a"
+	receiveId := map[string]string{"asr": "oc_aa573b380df9e8fde91a106f772dea35", "ocr": "oc_4c9f691580dee41d3b6e63643182e14c",
+		"nlp": "oc_532f1e2b627dd390434eba76cbe2b79a", "wenda": "oc_ca3ca124143d8c10fd6cdc4c078aee10",
+		"stamp": "oc_4c9f691580dee41d3b6e63643182e14c",
+	}
+	botUrl := "http://10.201.218.30:8501/send_to_feishu/send_text"
 	values := url.Values{}
-	values.Add("receive_id", "oc_xxxxxxxxx")
-	values.Add("text", "<at user_id=\\\\\\\"all\\\\\\\">ALL</at> "+k+" exception: "+msg+", URL: "+v+" \\\\n Please Check")
+	//values.Add("receive_id", "oc_ca3ca124143d8c10fd6cdc4c078aee10") // wenda
+	//values.Add("receive_id", "oc_532f1e2b627dd390434eba76cbe2b79a") // nlp
+	//values.Add("receive_id", "oc_bbaa1949e2267b73c3cc32e2e36d448d") // ocr
+	//values.Add("receive_id", "oc_aa573b380df9e8fde91a106f772dea35") // asr
+	values.Add("receive_id", receiveId[k])
+	values.Add("text", "<at user_id=\\\\\\\"all\\\\\\\">ALL</at> "+"\\\\n "+k+" "+msg+",\\\\n url: "+v+", \\\\n please check")
 	client := util.GetHttpClient()
 	//client.Post(url+param, "application/json", bytes.NewReader([]byte(jsonString)))
 	resp, err := client.PostForm(botUrl, values)
 	if resp != nil {
 		defer resp.Body.Close()
 	}
-	if err != nil || resp.StatusCode != http.StatusOK {
-		fmt.Println(err)
-		fmt.Errorf("send msg to bot error %s : %s", v, resp.Status)
+	if err != nil {
+		fmt.Printf("%v %v \n", k, err)
+		//fmt.Errorf("failed to fetch URL %s : %s", v, resp.Status)
 		return
 	}
+	status := resp.StatusCode
+	if status != http.StatusOK {
+		fmt.Printf("failed to fetch URL %s : %s", v, resp.Status)
+		//fmt.Errorf("send msg to bot error %s : %s", v, resp.Status)
+		//return
+	}
 
-	body, err := io.ReadAll(resp.Body)
+	//body, err := io.ReadAll(resp.Body)
+	_, err = io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("%s, send to msg to bot %s \n", time.Now().Format(tf), string(body))
+
+	fmt.Printf("%s, send to msg to bot %s \n", time.Now().Format(tf), k+" "+msg+" "+v)
 
 }
