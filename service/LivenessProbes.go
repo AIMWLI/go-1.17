4e161ee422d5b80c5ff684bd14955fb53f90505a
songjin <jin17.song@gmail.com>
2024-01-12 09:19:17 +0800

    info: remove log

--- service/LivenessProbes.go
+++ service/LivenessProbes.go
@@ -19,17 +19,18 @@ var resArray = mapset.NewSet("detection", "ai-nlp up", "pong", "hello world", "o
 func StartLivenessProbesJob() {
 	kv := setting.ProbesSetting.KV
 	c := cron.New(cron.WithSeconds())
-	c.AddFunc("@every 5s", func() {
+	c.AddFunc("@every 3s", func() {
 		client := util.GetHttpClient()
 		for k, v := range kv {
 			//fmt.Printf("Address of i=%d:\t%p\n", client, &client)
 			resp, err := client.Get(v)
-			if resp != nil {
-				defer resp.Body.Close()
-			}
 			if err != nil {
 				//fmt.Printf("%v %v", k, err)
-				_ = fmt.Errorf("failed to fetch URL %s : %s", v, resp.Status)
+				fmt.Printf("failed to fetch URL %s : %s \n", v, resp.Status)
+				return
+			}
+			if resp != nil {
+				defer resp.Body.Close()
 			}
 			if resp.StatusCode != http.StatusOK {
 				SendMsgToBot("探针检测异常", k, v)
@@ -51,7 +52,6 @@ func StartLivenessProbesJob() {
 }
 
 func SendMsgToBot(msg string, k string, v string) {
-	//receiveId["nlp"] = "oc_532f1e2b627dd390434eba76cbe2b79a"
 	receiveId := map[string]string{"asr": "111", "ocr": "222",
 		"nlp": "333", "wenda": "444",
 		"stamp": "555",
@@ -68,12 +68,11 @@ func SendMsgToBot(msg string, k string, v string) {
 	}
 	if err != nil {
 		fmt.Printf("%v %v \n", k, err)
-		//fmt.Errorf("failed to fetch URL %s : %s", v, resp.Status)
 		return
 	}
 	status := resp.StatusCode
 	if status != http.StatusOK {
-		fmt.Printf("failed to fetch URL %s : %s", v, resp.Status)
+		fmt.Printf("failed to fetch URL %s : %s \n", v, status)
 		//fmt.Errorf("send msg to bot error %s : %s", v, resp.Status)
 		//return
 	}
